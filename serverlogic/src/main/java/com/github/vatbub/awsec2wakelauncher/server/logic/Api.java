724c1562ef0dcc18673ec508c0fbaad709131fa7
Frederik Kammel <vatbub123+github@googlemail.com>
2018-05-03 17:59:49 +0200

    Fix: The api now does not return an empty dns string if none exists

--- serverlogic/src/main/java/com/github/vatbub/awsec2wakelauncher/server/logic/Api.java
+++ serverlogic/src/main/java/com/github/vatbub/awsec2wakelauncher/server/logic/Api.java
@@ -9,9 +9,9 @@ package com.github.vatbub.awsec2wakelauncher.server.logic;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -46,8 +46,8 @@ import java.util.regex.Pattern;
 
 
 public class Api extends HttpServlet {
-    private AwsInstanceManager awsInstanceManager;
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private AwsInstanceManager awsInstanceManager;
 
     @Override
     public void init() throws ServletException {
@@ -119,7 +119,9 @@ public class Api extends HttpServlet {
                 InstanceState instanceState = getAwsInstanceManager().getInstanceState(wakeRequest.getInstanceId());
 
                 wakeResponse.setPreviousInstanceState(instanceState.getCode());
-                wakeResponse.setInstanceDns(awsInstanceManager.getInstanceDns(wakeRequest.getInstanceId()));
+                String instanceDns = awsInstanceManager.getInstanceDns(wakeRequest.getInstanceId());
+                if (!instanceDns.isEmpty())
+                    wakeResponse.setInstanceDns(instanceDns);
                 wakeResponse.setInstanceIp(awsInstanceManager.getInstanceIp(wakeRequest.getInstanceId()));
 
                 if (instanceState.getCode() == 80)
